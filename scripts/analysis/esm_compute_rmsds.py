531a38d23077481b97fff682da8723f230c5614f
alexjli <alexjli@berkeley.edu>
2025-05-24 13:07:33 -0700

    update rmsd script

--- scripts/analysis/esm_compute_rmsds.py
+++ scripts/analysis/esm_compute_rmsds.py
@@ -25,7 +25,7 @@ from proteinzen.utils.openfold.tensor_utils import tree_map, tensor_tree_map
 from io import StringIO
 
 RMSD_ALL_HEAVY_TYPE = pyrosetta.rosetta.core.scoring.rmsd_atoms.rmsd_all_heavy
-RMSD_PROTEIN_BB_CA_TYPE = pyrosetta.rosetta.core.scoring.rmsd_atoms.rmsd_protein_bb_ca
+RMSD_PROTEIN_BB_HEAVY_TYPE = pyrosetta.rosetta.core.scoring.rmsd_atoms.rmsd_protein_bb_heavy
 
 
 def atom14_to_atom37_np(atom14, batch):
@@ -90,14 +90,35 @@ def parse_dict_to_df(d):
     return pd.DataFrame.from_dict(pd_dict)
 
 
-def compute_rmsds(design_path, folded_path, pmpnn_paths, fixed_res, fixed_res_chain):
+def compute_rmsds(
+    design_path,
+    folded_path,
+    pmpnn_paths,
+    fixed_seq,
+    fixed_seq_chain,
+    fixed_bb,
+    fixed_bb_chain
+):
     design_pose = pyrosetta.pose_from_pdb(design_path)
 
     fixed_res_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
     all_res_selector = pyrosetta.rosetta.core.select.residue_selector.TrueResidueSelector()
-    pdb_resids = [f"{resid}{chain}" for resid, chain in zip(fixed_res, fixed_res_chain)]
+    pdb_resids = [f"{resid}{chain}" for resid, chain in zip(fixed_seq, fixed_seq_chain)]
     res_str = ",".join(pdb_resids)
-    fixed_res_selector.set_index(res_str)
+    bb_resids = [f"{resid}{chain}" for resid, chain in zip(fixed_bb, fixed_bb_chain)]
+    bb_str = ",".join(bb_resids)
+
+    if len(res_str) == 0 and len(bb_str) > 0:
+        fixed_res_selector.set_index(bb_str)
+    else:
+        fixed_res_selector.set_index(res_str)
+
+    fixed_bb_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
+    if len(bb_str) > 0:
+        fixed_bb_selector.set_index(bb_str)
+    else:
+        fixed_bb_selector = None
+    print(res_str, bb_str)
 
     rmsd_metric = RMSDMetric()
     rmsd_metric.set_comparison_pose(design_pose)
@@ -115,36 +136,59 @@ def compute_rmsds(design_path, folded_path, pmpnn_paths, fixed_res, fixed_res_ch
         folded_pose = pyrosetta.Pose()
         pyrosetta.rosetta.core.import_pose.pose_from_pdbstring(folded_pose, fp.read())
 
-    rmsd_metric.set_rmsd_type(RMSD_ALL_HEAVY_TYPE)
-    rmsd_metric.set_residue_selector_super(fixed_res_selector)
-    # rmsd_metric.set_corresponding_atoms_robust(False)
+    try:
+        rmsd_metric.set_rmsd_type(RMSD_ALL_HEAVY_TYPE)
+        rmsd_metric.set_residue_selector(fixed_res_selector)
+        rmsd_metric.set_residue_selector_super(fixed_res_selector)
+        design_data['motif_all_atom_rmsd'] = rmsd_metric.calculate(folded_pose)
+        rmsd_metric.set_residue_selector(all_res_selector)
+        rmsd_metric.set_residue_selector_super(all_res_selector)
+        design_data['global_all_atom_rmsd'] = rmsd_metric.calculate(folded_pose)
 
-    rmsd_metric.set_residue_selector(fixed_res_selector)
-    design_data['motif_all_atom_rmsd'] = rmsd_metric.calculate(folded_pose)
-    rmsd_metric.set_residue_selector(all_res_selector)
-    design_data['global_all_atom_rmsd'] = rmsd_metric.calculate(folded_pose)
+        rmsd_metric.set_rmsd_type(RMSD_PROTEIN_BB_HEAVY_TYPE)
+        if fixed_bb_selector is not None:
+            rmsd_metric.set_residue_selector(fixed_bb_selector)
+            rmsd_metric.set_residue_selector_super(fixed_bb_selector)
+        design_data['motif_bb_rmsd'] = rmsd_metric.calculate(folded_pose)
+        rmsd_metric.set_residue_selector(all_res_selector)
+        rmsd_metric.set_residue_selector_super(all_res_selector)
+        design_data['global_bb_rmsd'] = rmsd_metric.calculate(folded_pose)
+    except Exception as e:
+        print(f"got error {e}, for path {folded_path}, assigning rmsds to 10")
+        design_data['motif_all_atom_rmsd'] = 10.
+        design_data['global_all_atom_rmsd'] = 10.
+        design_data['motif_bb_rmsd'] = 10.
+        design_data['global_bb_rmsd'] = 10.
 
-    rmsd_metric.set_rmsd_type(RMSD_PROTEIN_BB_CA_TYPE)
-    rmsd_metric.set_residue_selector(fixed_res_selector)
-    design_data['motif_ca_rmsd'] = rmsd_metric.calculate(folded_pose)
-    rmsd_metric.set_residue_selector(all_res_selector)
-    design_data['global_ca_rmsd'] = rmsd_metric.calculate(folded_pose)
 
     pmpnn_data = []
     # rmsd_metric.set_corresponding_atoms_robust(True)
     for path in pmpnn_paths:
-        print(path)
+        # print(path)
         _data = {}
-        with open(path) as fp:
-            pmpnn_pose = pyrosetta.Pose()
-            pyrosetta.rosetta.core.import_pose.pose_from_pdbstring(pmpnn_pose, fp.read())
-        rmsd_metric.set_rmsd_type(RMSD_ALL_HEAVY_TYPE)
-        rmsd_metric.set_residue_selector(fixed_res_selector)
-        _data['motif_all_atom_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
-        rmsd_metric.set_rmsd_type(RMSD_PROTEIN_BB_CA_TYPE)
-        _data['motif_ca_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
-        rmsd_metric.set_residue_selector(all_res_selector)
-        _data['global_ca_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
+        try:
+            with open(path) as fp:
+                pmpnn_pose = pyrosetta.Pose()
+                pyrosetta.rosetta.core.import_pose.pose_from_pdbstring(pmpnn_pose, fp.read())
+            rmsd_metric.set_rmsd_type(RMSD_ALL_HEAVY_TYPE)
+            rmsd_metric.set_residue_selector(fixed_res_selector)
+            rmsd_metric.set_residue_selector_super(fixed_res_selector)
+            _data['motif_all_atom_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
+            rmsd_metric.set_rmsd_type(RMSD_PROTEIN_BB_HEAVY_TYPE)
+            if fixed_bb_selector is not None:
+                rmsd_metric.set_residue_selector(fixed_bb_selector)
+                rmsd_metric.set_residue_selector_super(fixed_bb_selector)
+            _data['motif_bb_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
+            rmsd_metric.set_residue_selector(all_res_selector)
+            rmsd_metric.set_residue_selector_super(all_res_selector)
+            _data['global_bb_rmsd'] = rmsd_metric.calculate(pmpnn_pose)
+        except Exception as e:
+            print(f"got error {e}, for path {path}, assigning rmsds to 10")
+            _data = {
+                'motif_all_atom_rmsd': 10.,
+                'motif_bb_rmsd': 10.,
+                'global_bb_rmsd': 10.,
+            }
         pmpnn_data.append(_data)
 
     return design_data, pmpnn_data
@@ -189,10 +233,21 @@ if __name__ == '__main__':
                 pmpnn_paths.append(path)
                 pmpnn_paths_idx.append(sample_num)
 
-        fixed_res_idx = samples_metadata[name]['fixed_res_idx']
-        fixed_res_chain = samples_metadata[name]['fixed_res_chain']
+        fixed_seq_idx = samples_metadata[name]['fixed_seq_res_idx']
+        fixed_seq_chain = samples_metadata[name]['fixed_seq_chain']
+        fixed_bb_res_idx = samples_metadata[name]['fixed_bb_res_idx']
+        fixed_bb_chain = samples_metadata[name]['fixed_bb_chain']
+
         try:
-            design_rmsds, pmpnn_rmsds = compute_rmsds(sample_path, design_folded_path, pmpnn_paths, fixed_res_idx, fixed_res_chain)
+            design_rmsds, pmpnn_rmsds = compute_rmsds(
+                sample_path,
+                design_folded_path,
+                pmpnn_paths,
+                fixed_seq_idx,
+                fixed_seq_chain,
+                fixed_bb_res_idx,
+                fixed_bb_chain,
+            )
             print(design_rmsds)
             print(pmpnn_rmsds)
         except Exception as e:
@@ -231,17 +286,25 @@ if __name__ == '__main__':
         sub_df = sc_df[sc_df['name'] == name]
         motif_min_rmsd = sub_df['motif_all_atom_rmsd'].min()
         collapsed_motif_df.append(sub_df[sub_df['motif_all_atom_rmsd'] == motif_min_rmsd])
-        global_min_rmsd = sub_df['global_ca_rmsd'].min()
-        collapsed_global_df.append(sub_df[sub_df['global_ca_rmsd'] == global_min_rmsd])
+        global_min_rmsd = sub_df['global_bb_rmsd'].min()
+        collapsed_global_df.append(sub_df[sub_df['global_bb_rmsd'] == global_min_rmsd])
     collapsed_motif_df = pd.concat(collapsed_motif_df)
     collapsed_motif_df.to_csv("best_sc_motif_rmsd.csv")
     collapsed_global_df = pd.concat(collapsed_global_df)
     collapsed_global_df.to_csv("best_sc_global_rmsd.csv")
 
-    pmpnn_pass = sc_df[(sc_df['motif_all_atom_rmsd'] < 1) & (sc_df['global_ca_rmsd'] < 2)]
+    pmpnn_pass = sc_df[
+        (sc_df['motif_all_atom_rmsd'] < 1.5)
+        & (sc_df['motif_bb_rmsd'] < 1)
+        & (sc_df['global_bb_rmsd'] < 2)
+    ]
     pmpnn_pass = pmpnn_pass.groupby("name").sample(1)
     pmpnn_task_pass = pmpnn_pass.groupby("task")
-    pz_pass = folding_df[(folding_df['motif_all_atom_rmsd'] < 1) & (folding_df['global_all_atom_rmsd'] < 2)]
+    pz_pass = folding_df[
+        (folding_df['motif_all_atom_rmsd'] < 1.5)
+        & (folding_df['motif_bb_rmsd'] < 1)
+        & (folding_df['global_all_atom_rmsd'] < 2)
+    ]
     pz_task_pass = pz_pass.groupby("task")
 
     with open("../num_designable.txt", 'w') as fp:
