05c82f8859bc23887c0f5dc32631e186a525f17f
David Yang <dyang20@cs.utexas.edu>
2026-03-11 17:33:21 -0500

    update sample run

--- sample_conformer.sh
+++ sample_conformer.sh
@@ -1,7 +1,10 @@
+MODE=train
+MODEL_NAME=geom_identityRot_256_conformer_3std_bondlength
+
 python sample.py \
-    model_dir=/datastor1/dy4652/proteinzen/outputs/geom_identityRot_256_conformer_3std_bondlength/train \
-    out_dir=sampling/geom_conformer_train/geom_identityRot_256_conformer_train_3std_bondlength \
-    sampler.tasks_yaml=sampling/geom_conformer_train/smiles.yaml \
+    model_dir=/datastor1/dy4652/proteinzen/outputs/${MODEL_NAME}/train \
+    out_dir=sampling/geom_conformer_${MODE}/${MODEL_NAME}_0_5_trans_vf_scale \
+    sampler.tasks_yaml=sampling/geom_conformer_${MODE}/smiles.yaml \
     sampler.batch_size=32 \
     save_traj=true \
     +version_num=29 \
