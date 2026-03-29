5115851b6f588ba3b13e2e6bb7f8a829de96c00b
David Yang <dyang20@cs.utexas.edu>
2026-03-29 05:12:13 -0500

    geom_conformer slurm 2

--- scripts/data/gen_geom_conformer.sh
+++ scripts/data/gen_geom_conformer.sh
@@ -1,7 +1,7 @@
 dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 source $dir/../../env_vars.sh 
 
-python geom_conformer.py \
+python ${REPO_ROOT}/scripts/data/geom_conformer.py \
   --datadir "${REPO_ROOT}/data/rdkit/" \
   --dataset drugs \
   --outdir "${REPO_ROOT}/data/geom_drugs_conformers/" \
