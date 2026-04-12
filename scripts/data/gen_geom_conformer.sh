2e959bf2e495a545f8d47f18b0474aa0edb2874a
David Yang <dyang20@cs.utexas.edu>
2026-04-12 08:27:34 -0500

    energy openmp maybe fix

--- scripts/data/gen_geom_conformer.sh
+++ scripts/data/gen_geom_conformer.sh
@@ -1,5 +1,7 @@
 dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-source $dir/../../env_vars.sh 
+source $dir/../../env_vars.sh
+
+export OMP_NUM_THREADS=1
 
 python ${REPO_ROOT}/scripts/data/geom_conformer.py \
   --datadir "${REPO_ROOT}/data/rdkit/" \
