bd64efd89ce655988952ed7177c5065da6903296
David Yang <dyang20@cs.utexas.edu>
2026-07-06 04:17:01 -0500

    _recursive_=False

--- scripts/test_equivariance.py
+++ scripts/test_equivariance.py
@@ -112,7 +112,7 @@ def main():
 
     print(f"Instantiating model from {args.hydra_config}...")
     cfg = omegaconf.OmegaConf.load(args.hydra_config)
-    lmod = instantiate(cfg.lmodule, _recursive_=False)
+    lmod = instantiate(cfg.lmodule)
 
     print(f"Loading weights from {args.ckpt}...")
     ckpt = torch.load(args.ckpt, map_location=device, weights_only=False)
